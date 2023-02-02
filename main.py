5f1660a128c4b0d849c852773b88c892bc26bf45
Oleh Voloshyn <bazisc.qa@gmail.com>
2023-02-03 00:19:49 +0200

    remove health

--- main.py
+++ main.py
@@ -24,19 +24,4 @@ async def webhook(req: Request):
 
 @app.post("/ping")
 async def ping():
-    last_ping = db.health.get("admin")
-
-    def update_health():
-        db.health.put({
-            "last_message_id": msg.message_id,
-            "last_update": str(datetime.now(tz=pytz.timezone("Europe/Kiev")))
-        }, "admin")
-
-    if last_ping:
-        bot.delete_message(HEARTBEAT_CHAT_ID, last_ping["last_message_id"])
-    try:
-        msg = bot.send_message(HEARTBEAT_CHAT_ID, "pong")
-        if msg is not None:
-            update_health()
-    except:
-        pass
+    return "pong"
